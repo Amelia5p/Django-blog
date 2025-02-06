592f7ea2c1b496f52157096ec62df9eea1f30b96
Amelia Service <ekliwulj@students.codeinstitute.net>
2025-02-06 12:28:30 +0000

    debug true

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = ('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-amelia5p-djangoblog-9o46wodnwtn.ws.codeinstitute-ide.net', '.herokuapp.com']
 
